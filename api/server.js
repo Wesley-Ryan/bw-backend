dc62c10f5a500e424e376af07876c424fe0748d9
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-12-23 20:55:07 -0800

    morgan loggin

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const morgan = require("morgan");
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
+server.use(morgan("common"));
 server.use(express.json());
 
 server.get("/", (_, res) => {
