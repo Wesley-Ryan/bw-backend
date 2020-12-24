ab7ba772c1593116f51ad5845a50fc1b13a19c1b
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-12-23 21:03:33 -0800

    initial migration file

---
+++ data/migrations/20201223205534_users.js
@@ -0,0 +1,24 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("username", 255).notNullable().unique();
+      users.string("password", 255).notNullable();
+      users
+        .integer("role")
+        .unsigned()
+        .notNullable()
+        .references("role_id")
+        .inTable("roles")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    })
+    .createTable("roles", (roles) => {
+      roles.increments("role_id");
+      roles.string("name", 255).notNullable().unique();
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users");
+};
